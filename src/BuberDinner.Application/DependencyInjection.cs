16b244758caaddfa839762ce8230fb033d504248
Bogdan K <bkalinovski@mail.ru>
2023-12-29 16:06:18 +0200

    add validation pipeline into application layer

--- src/BuberDinner.Application/DependencyInjection.cs
+++ src/BuberDinner.Application/DependencyInjection.cs
@@ -1,4 +1,10 @@
 using System.Reflection;
+using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Common.Behaviors;
+using MediatR;
+using ErrorOr;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BuberDinner.Application;
@@ -8,6 +14,10 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         
         return services;
     }
